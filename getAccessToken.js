732a1bde740950f2950b4a0329a3c588bf6e9fed
MasumiShiraiwa <masumi@rcl-aoyama.jp>
2025-02-26 09:34:20 +0900

    copying sample codes

---
+++ getAccessToken.js
@@ -0,0 +1,57 @@
+const jwt = require('jsonwebtoken');
+const axios = require("axios");
+
+/**
+ * Generate JWT for access token
+ * @param {string} clientId - Client ID
+ * @param {string} serviceAccount - Service Account
+ * @param {string} privatekey - Private Key
+ * @return {string} JWT
+ */
+let getJWT = (clientId, serviceAccount, privatekey) => {
+    current_time = Date.now() / 1000;
+    iss = clientId;
+    sub = serviceAccount;
+    iat = current_time;
+    exp = current_time + (60 * 60); // 1 hour
+
+    jws = jwt.sign(
+        {
+            "iss": iss,
+            "sub": sub,
+            "iat": iat,
+            "exp": exp
+        }, privatekey, {algorithm: "RS256"});
+
+    return jws;
+};
+
+
+/**
+ * Get Access Token
+ * @async
+ * @param {string} clientId - Client ID
+ * @param {string} clientSecret - Client Secret
+ * @param {string} serviceAccount - Service Account
+ * @param {string} privatekey - Private Key
+ * @param {string} scope - OAuth Scope
+ * @return {string} Access Token
+ */
+let getAccessToken = async (clientId, clientSecret, serviceAccount, privatekey, scope) => {
+    const jwt = getJWT(clientId, serviceAccount, privatekey);
+
+    const params = new URLSearchParams({
+        assertion: jwt,
+        grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+        client_id: clientId,
+        client_secret: clientSecret,
+        scope: scope,
+    });
+
+    const res = await axios.post("https://auth.worksmobile.com/oauth2/v2.0/token", params);
+
+    const accessToken = res.data.access_token;
+
+    return accessToken;
+};
+
---
+++ validateRequest.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * トークルームで発生したイベントにより、Callbackイベントのリクエストを受け取った後、
+ * Request headerの X-WORKS-Signature によって渡される署名(signature)と、
+ * Developer ConsoleのBot画面で発行されている "Bot Secret"(環境変数) を用いて、Request bodyの改ざんチェックを行う。
+ */
+
+var crypto = require("crypto");
+
+
+let safeCompare = (a, b) => {
+    if (a.length !== b.length) {
+        return false;
+    }
+    return crypto.timingSafeEqual(a, b);
+};
+
+
+/**
+ * Validate request
+ * @param {Object} body - Request Body
+ * @param {string} signature - value of X-WORKS-Signature header
+ * @param  {string} botSecret - Bot Secret
+ * @return {boolean} is valid
+ */
+let validateRequest = (body, signature, botSecret) => {
+    return safeCompare(
+        crypto.createHmac("SHA256", botSecret).update(body).digest(),
+        Buffer.from(signature, "base64"),
+    );
+};
+
