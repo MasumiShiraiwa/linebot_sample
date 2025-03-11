e1d736da1d752820e5a026d50356a8660d1f6416
MasumiShiraiwa <masumi@rcl-aoyama.jp>
2025-03-11 23:20:29 +0900

    add get task categories list func

--- getTask.js
+++ getTask.js
@@ -1,6 +1,23 @@
 const axios = require("axios");
 
 
+let getTaskCategoryList = async (userId, accessToken) => {
+    console.log("get task category list");
+    const headers = {
+        Authorization: `Bearer ${accessToken}`
+    };
+    try{
+        const res = await axios.get(`https://www.worksapis.com/v1.0/users/${userId}/task-categories`, {headers});
+        return res.data;
+    }catch(e){
+        console.error("Error getting task category list:", e.message);
+        if(e.response){
+            console.error("HTTP Status:", e.response.status);   
+            console.error("Response Data:", e.response.data);
+        }
+    }
+}
+
 let getTaskList = async (userId, accessToken) => {
     console.log("get task list");
     console.log("type of userId: ", typeof(userId))
@@ -8,7 +25,7 @@ let getTaskList = async (userId, accessToken) => {
         Authorization: `Bearer ${accessToken}`
     };
     const params = {
-        categoryId: "0"
+        categoryId: "0" // ここが違う
     }
     try{
         const res = await axios.get(`https://www.worksapis.com/v1.0/users/${userId}/tasks`, {headers, params: params});
@@ -22,6 +39,29 @@ let getTaskList = async (userId, accessToken) => {
     }
 };
 
+// let postTask = async (userId, accessToken) => {
+//     console.log("post task");
+//     const headers = {
+//         Authorization: `Bearer ${accessToken}`
+//     };
+//     const params = {
+//         "assignorId": userId,
+//         "assignees": [
+//           {
+//             "assigneeId": userId,
+//             "status": "TODO"
+//           }
+//         ],
+//         "title": "週次会議の予定",
+//         "content": "会議室の予約",
+//         "dueDate": "2024-05-25",
+//         "completionCondition": "ANY_ONE",
+//         "categoryId": "7f5078df-bf67-417c-a559-f5539e59bd0d"
+//       }
+    
+// }
+
 module.exports = {
+    getTaskCategoryList,
     getTaskList
 };
