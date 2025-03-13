93eb88b8d5069bf51a82170f2ba32bfa9cbc900b
MasumiShiraiwa <masumi@rcl-aoyama.jp>
2025-03-14 00:24:39 +0900

    escape

--- handleGoogleDrive.js
+++ handleGoogleDrive.js
@@ -12,7 +12,7 @@ let authorize = async () => {
         scopes: SCOPES,
         credentials: {
             client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY
+            private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
         }
     });
     const drive = google.drive({version: "v3", auth});
