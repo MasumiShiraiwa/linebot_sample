ab7ea81fb221a33eef57b778330a136be1b295cc
MasumiShiraiwa <masumi@rcl-aoyama.jp>
2025-03-15 00:39:43 +0900

    change authorization method

--- handleGoogleDrive.js
+++ handleGoogleDrive.js
@@ -9,22 +9,22 @@ let authorize = async () => {
 
     console.log("--------------------------------");
     console.log("Get Access Token");
-    // const auth = new google.auth.JWT(
-    //     process.env.GOOGLE_CLIENT_EMAIL,
-    //     null,
-    //     process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // 改行処理
-    //     SCOPES
-    // );
+    const auth = new google.auth.JWT(
+        process.env.GOOGLE_CLIENT_EMAIL,
+        null,
+        process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // 改行処理
+        SCOPES
+    );
     
 
     // const drive = google.drive({version: "v3", auth});
-    const auth = new google.auth.GoogleAuth({
-        scopes: SCOPES,
-        credentials: {
-            client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
-        },
-    });
+    // const auth = new google.auth.GoogleAuth({
+    //     scopes: SCOPES,
+    //     credentials: {
+    //         client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    //         private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    //     },
+    // });
     
     const authClient = await auth.getClient();
     const drive = google.drive({version: "v3", auth: authClient});
